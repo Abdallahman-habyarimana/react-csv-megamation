4f9ad135b692ababdcfe8bc8932cd807a302b536
Abdallahman Habyarimana <habdallah005@gmail.com>
2024-02-24 19:20:40 -0500

    display request on server with morgan

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,12 @@
-
-import React from 'react'
+import axios from 'axios'
+import { useEffect, useState } from 'react'
 
 const App = () => {
+  const [demandes, setDemandes] = useState([])
+  useEffect(() => {
+    const response = axios.get('http://localhost:8000/demandes').then((res) => console.log(res))
+    console.log(response);
+  }, [])
   return (
     <div>
       hello world
--- server/src/app.ts
+++ server/src/app.ts
@@ -1,12 +1,15 @@
 import express from 'express'
 import cors from 'cors';
 import demandeRouter from './routes/demande.router';
+import morgan from 'morgan'
 
 const app = express()
 
 app.use(cors({
     origin: 'http://localhost:5173'
-}))
+}));
+
+app.use(morgan('combined'));
 
 app.use(express.json())
 app.use(demandeRouter)
